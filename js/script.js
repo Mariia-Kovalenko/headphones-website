0c93a18e72c5b7633f0b3318f2f5ea86bc3ce6c2
Mariia-Kovalenko <mariya.kovalenko.2000@gmail.com>
2022-05-04 12:53:50 +0300

    add function to toggle display

--- js/script.js
+++ js/script.js
@@ -60,8 +60,21 @@ window.addEventListener('DOMContentLoaded', () => {
         renderTheme(localStorage.getItem('theme'));
     }
 
+
+    function toggleDisplay(itemShow, itemHide = null) {
+        if(itemShow.classList.length === 0 || itemShow.classList.contains('hide')){
+            itemShow.classList.remove('hide');
+            itemShow.classList.add('show');
+
+            if(itemHide) {
+                itemHide.classList.remove('show');
+                itemHide.classList.add('hide');
+            }
+        }
+    }
+
     function renderTheme(theme){
-        console.log(`theme is ${theme}`);
+        // console.log(`theme is ${theme}`);
         const logoLight = document.querySelector('#logo-light');
         const logoDark = document.querySelector('#logo-dark');
         const themeDarkBtn = document.querySelector('#theme-dark');
@@ -82,68 +95,22 @@ window.addEventListener('DOMContentLoaded', () => {
         const burgerLight = document.querySelector('#burger-light');
         const burgerDark = document.querySelector('#burger-dark');
 
-        moonLight.style.transitionDelay = '.4s';
-        moonDark.style.transitionDelay = '.4s';
-
         if(theme === 'dark'){
             if (bodyTag.classList.length == 0 || bodyTag.classList.contains('light-bg')){
                 bodyTag.classList.remove('dark-text');
                 bodyTag.classList.remove('light-bg');
+
                 bodyTag.classList.add('light-text');
                 bodyTag.classList.add('dark-bg');
             }
 
-            if(logoLight.classList.length === 0 || logoLight.classList.contains('hide')){
-                logoLight.classList.remove('hide');
-                logoLight.classList.add('show');
-
-                logoDark.classList.remove('show');
-                logoDark.classList.add('hide');
-            }
-
-            if (moonLight.classList.length === 0 || moonLight.classList.contains('hide')) {
-                moonLight.classList.remove('hide');
-                moonLight.classList.add('show');
-                moonDark.classList.remove('show');
-                moonDark.classList.add('hide');
-            }
-
-            if (sunLight.classList.length === 0 || sunLight.classList.contains('hide')) {
-                sunLight.classList.remove('hide');
-                sunLight.classList.add('show');
-            }
-
-            if (cartLight.classList.length === 0 || cartLight.classList.contains('hide')) {
-                cartLight.classList.remove('hide');
-                cartLight.classList.add('show');
-
-                cartDark.classList.remove('show');
-                cartDark.classList.add('hide');
-            }
-
-            if (profileLight.classList.length === 0 || profileLight.classList.contains('hide')) {
-                profileLight.classList.remove('hide');
-                profileLight.classList.add('show');
-
-                profileDark.classList.remove('show');
-                profileDark.classList.add('hide');
-            }
-
-            if (burgerLight.classList.length === 0 || burgerLight.classList.contains('hide')) {
-                burgerLight.classList.remove('hide');
-                burgerLight.classList.add('show');
-
-                burgerDark.classList.remove('show');
-                burgerDark.classList.add('hide');
-            }
-
-            if (closeLight.classList.length === 0 || closeLight.classList.contains('hide')) {
-                closeLight.classList.remove('hide');
-                closeLight.classList.add('show');
-
-                closeDark.classList.remove('show');
-                closeDark.classList.add('hide');
-            }
+            toggleDisplay(logoLight, logoDark);
+            toggleDisplay(moonLight, moonDark);
+            toggleDisplay(sunLight);
+            toggleDisplay(cartLight, cartDark);
+            toggleDisplay(profileLight, profileDark);
+            toggleDisplay(burgerLight, burgerDark);
+            toggleDisplay(closeLight, closeDark);
 
             if (!themeDarkBtn.classList.contains('theme-active')){
                 themeDarkBtn.classList.add('theme-active');
@@ -162,56 +129,13 @@ window.addEventListener('DOMContentLoaded', () => {
                 bodyTag.classList.add('light-bg');
             }
 
-            if(logoLight.classList.length === 0 || logoDark.classList.contains('hide')){
-                logoDark.classList.remove('hide');
-                logoDark.classList.add('show');
-                logoLight.classList.remove('show');
-                logoLight.classList.add('hide');
-            }
-
-            if (moonDark.classList.length === 0 || moonDark.classList.contains('hide')) {
-                moonDark.classList.remove('hide');
-                moonDark.classList.add('show');
-                moonLight.classList.remove('show');
-                moonLight.classList.add('hide');
-            }
-
-            if (sunLight.classList.length === 0 || sunLight.classList.contains('hide')) {
-                sunLight.classList.remove('hide');
-                sunLight.classList.add('show');
-            }
-
-            if (cartDark.classList.length === 0 || cartDark.classList.contains('hide')) {
-                cartDark.classList.remove('hide');
-                cartDark.classList.add('show');
-
-                cartLight.classList.remove('show');
-                cartLight.classList.add('hide');
-            }
-
-            if (profileDark.classList.length === 0 || profileDark.classList.contains('hide')) {
-                profileDark.classList.remove('hide');
-                profileDark.classList.add('show');
-
-                profileLight.classList.remove('show');
-                profileLight.classList.add('hide');
-            }
-
-            if (burgerDark.classList.length === 0 || burgerDark.classList.contains('hide')) {
-                burgerDark.classList.remove('hide');
-                burgerDark.classList.add('show');
-
-                burgerLight.classList.remove('show');
-                burgerLight.classList.add('hide');
-            }
-
-            if (closeDark.classList.length === 0 || closeDark.classList.contains('hide')) {
-                closeDark.classList.remove('hide');
-                closeDark.classList.add('show');
-
-                closeLight.classList.remove('show');
-                closeLight.classList.add('hide');
-            }
+            toggleDisplay(logoDark, logoLight);
+            toggleDisplay(moonDark, moonLight);
+            toggleDisplay(sunLight);
+            toggleDisplay(cartDark, cartLight);
+            toggleDisplay(profileDark, profileLight);
+            toggleDisplay(burgerDark, burgerLight);
+            toggleDisplay(closeDark, closeLight);   
 
             if (!themeLightBtn.classList.contains('theme-active')){
                 themeLightBtn.classList.add('theme-active');
